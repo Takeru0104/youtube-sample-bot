d489bfb96a8622f2b79c0742430aac9ce15af4c5
Takeru0104 <101442306+Takeru0104@users.noreply.github.com>
2022-04-24 08:09:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    main();
+    line_bot_api.reply_message(
+        main();
+    )
+    
 
 def main():
   USER_ID = info['USER_ID']
