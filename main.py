84a978b5779a2e3ed6177d27bc326086cd472805
Takeru0104 <101442306+Takeru0104@users.noreply.github.com>
2022-04-24 07:36:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ info = json.load(file)
 CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
+def main():
+  USER_ID = info['USER_ID']
+  messages=TextSendMessage(text="おはよう")
+  line_bot_api.push_message(USER_ID , messages=messages)
+if __name__ == "__main__":
+  main()
