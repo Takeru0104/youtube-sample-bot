d73302f3e4bf222530d06af66b5919f9f51383da
Takeru0104 <101442306+Takeru0104@users.noreply.github.com>
2022-04-24 08:11:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ info = json.load(file)
 
 CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+
+'''
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         main();
     )
     
-
+'''
 def main():
   USER_ID = info['USER_ID']
   messages=TextSendMessage(text="おはよう")
