3d60229b2c46d48763a16cb4d1df69b3912b039d
Takeru0104 <101442306+Takeru0104@users.noreply.github.com>
2022-04-25 18:02:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -14,7 +15,7 @@ info = json.load(file)
 CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/callback", methods=['POST'])
 def callback():
